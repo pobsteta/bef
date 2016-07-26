c880614a80dca54d6220f955c47cab244286a0d9
pobsteta <pascal.obstetar@gmail.com>
2016-07-26 16:18:21 +0200

    maj all files

--- bef-db/init-pg.sh
+++ bef-db/init-pg.sh
@@ -32,7 +32,7 @@ if [ "$1" = 'postgres' ]; then
 				                accès au port Postgres d'accéder à 
 				                votre base de données. Utilisez 
 				                "POSTGRES_PASSWORD -e = mot de passe"
-				                pour définir le "docker run".				
+				                pour définir le "docker run".
 				****************************************************
 			EOWARN
 
@@ -42,7 +42,7 @@ if [ "$1" = 'postgres' ]; then
 
 		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
 
-		# internal start of server in order to allow set-up using psql-client		
+		# internal start of server in order to allow set-up using psql-client
 		# does not listen on external TCP/IP and waits until start finishes
 		gosu postgres pg_ctl -D "$PGDATA" \
 			-o "-c listen_addresses='*'" \
--- bef-db/init-postgis-db.sh
+++ bef-db/init-postgis-db.sh
@@ -25,47 +25,47 @@ EOSQL
 INIT="$(echo "$INIT" | sed -e 's/^[ \t]*//;s/[ \t]*$//')"
 
 if [ "${INIT}" == "0" ]; then    
-   	echo "Crée le rôle $GIS_USER..."
-	psql <<-EOSQL
-		CREATE ROLE $GIS_USER WITH LOGIN $PASS SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION;		
+    echo "Crée le rôle $GIS_USER..."
+    psql <<-EOSQL
+        CREATE ROLE $GIS_USER WITH LOGIN $PASS SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION;
 	EOSQL
 	echo
 	echo "Crée la base de données $GIS_DB..."
 	psql <<-EOSQL
-		CREATE DATABASE $GIS_DB WITH OWNER $GIS_USER ENCODING='$DB_ENCODING';		
+		CREATE DATABASE $GIS_DB WITH OWNER $GIS_USER ENCODING='$DB_ENCODING';
 	EOSQL
 	if [ -f "/data/restore/$GIS_DB.sql.gz" ]; then
-	    # Si un backup de tryton est dans la répertoire /data/restore
+        # Si un backup de tryton est dans la répertoire /data/restore
         # alors on restaure la BDD
         shopt -s nullglob
         for f in /data/restore/*.sql.gz
         do
-        	echo
-	        echo "Fichier de RESTAURATION trouvé : $f"
+            echo
+            echo "Fichier RESTAURATION trouvé : $f"
             DBNAME=$(basename -s ".sql.gz" "$f")
             echo
             echo "Restauration de la base de données $DBNAME..."
-            { gosu postgres gunzip -c "$f" | psql -q -d "$DBNAME"; }       
+            { gosu postgres gunzip -c "$f" | psql -q -d "$DBNAME"; }
             echo "Restauration exécutée !"
         done
 	else
 	    echo
-	    echo "Ajoute l'extension postgis à la base de données $GIS_DB..."	
+	    echo "Ajoute l'extension postgis à la base de données $GIS_DB..."
         psql --dbname $GIS_DB <<-EOSQL
 		    CREATE EXTENSION postgis;
 		EOSQL
-		echo	    
-	    echo "Ajoute l'extension postgis_topology à la base de données $GIS_DB..."	
+		echo
+	    echo "Ajoute l'extension postgis_topology à la base de données $GIS_DB..."
 	    psql --dbname $GIS_DB <<-EOSQL
 	        CREATE EXTENSION postgis_topology;
 		EOSQL
-		echo		
-		echo "Ajoute l'extension fuzzystrmatch à la base de données $GIS_DB..."	
+		echo
+		echo "Ajoute l'extension fuzzystrmatch à la base de données $GIS_DB..."
 		psql --dbname $GIS_DB <<-EOSQL
 			CREATE EXTENSION fuzzystrmatch;
 		EOSQL
 		echo
-		echo "Ajoute l'extension legacy à la base de données $GIS_DB..."	
+		echo "Ajoute l'extension legacy à la base de données $GIS_DB..."
 		psql --dbname $GIS_DB -q -1f $SQLDIR/legacy.sql
 	fi
 	echo
